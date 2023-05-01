1a7f13e6114b0788f7b991e0cdb46da17689e05e
quentin-eth-33 <quentin.bret29@gmail.com>
2023-05-01 15:00:47 +0200

    .

--- analyser/type_analyser.ml
+++ analyser/type_analyser.ml
@@ -1,104 +1,74 @@
 open Util
 open Ast
+
 let type_analyser report program =
   let type_environment = Environment.new_environment () in
   Environment.add type_environment "x" Type_int;
   Error_report.add_warning report
     ("sample_warning", (Lexing.dummy_pos, Lexing.dummy_pos));
 
-  let rec type_expression env expr =
-    match expr with
-      | Constant_i (i, annotation) -> Annotation.set_type annotation Type_int;(* Constant_i(i, set_type annotation Type_int)*)
-      | Constant_f (f, annotation) -> Annotation.set_type annotation Type_float;
-      | Constant_b (b, annotation) -> Annotation.set_type annotation Type_bool;
+    let rec type_expression env expr report =
+      match expr with
+      | Constant_i (i, annotation) -> Annotation.set_type annotation Type_int
+      | Constant_f (f, annotation) -> Annotation.set_type annotation Type_float
+      | Constant_b (b, annotation) -> Annotation.set_type annotation Type_bool
       | Pos (e1, e2, annotation) ->
-        let te1 = type_expression env e1 in
-    let te2 = type_expression env e2 in
-    Annotation.set_type annotation Type_pos;
-        
-      | Point (e1, e2, _) ->
-        type_expression env e1;
-        type_expression env e2;
-        Type_point
-      | Color (e1, e2, e3, _) ->
-        type_expression env e1;
-        type_expression env e2;
-        type_expression env e3;
-        Type_color
-      | Variable (name, _) ->
-        Environment.find_variable_type env name
-      | Field_accessor (fa, e, _) ->
-        type_expression env e;
-        (match fa with
-         | Color_accessor -> Type_color
-         | Position_accessor -> Type_pos
-         | X_accessor | Y_accessor -> Type_int
-         | Blue_accessor | Red_accessor | Green_accessor -> Type_int)
-      | Binary_operator (op, e1, e2, _) ->
-        type_expression env e1;
-        type_expression env e2;
-        (match op with
-         | Add | Sub | Mul | Div | Mod -> Type_int
-         | And | Or | Lt | Gt | Le | Ge | Ne | Eq -> Type_bool)
-      | Unary_operator (op, e, _) ->
-        type_expression env e;
-        (match op with
-         | USub | Floor | Float_of_int | Cos | Sin -> Type_int
-         | Not | Head | Tail -> Type_bool)
-      | List (elems, _) ->
-        let rec list_type = function
-          | [] -> Type_list Type_null
-          | h :: t ->
-            let ht = type_expression env h in
-            let tt = list_type t in
-            if ht = tt then ht else raise (TypeError "Type mismatch in list")
-        in list_type elems
-      | Cons (e1, e2, _) ->
-        type_expression env e1;
-        type_expression env e2;
-        Type_list (type_expression env e1)
-    in
-    ann.type_expr <- Some te
-
-  and type_statement env stmt =
-    match stmt with
-    | Variable_declaration (name, typ, _) ->
-      Environment.add_variable env name typ
-    | Assignment (expr1, expr2, _) ->
-      type_expression env expr1;
-      type_expression env expr2
-    | Block (stmts, _) ->
-      let new_env = Environment.add_layer env in
-      List.iter (type_statement new_env) stmts;
-      Environment.remove_layer new_env
-    | IfThenElse (test, i1, i2, _) ->
-      type_expression env test;
-      type_statement env i1;
-      type_statement env i2
-    | For (id, start_expr, end_expr, step_expr, stmt, _) ->
-      type_expression env start_expr;
-      type_expression env end_expr;
-      type_expression env step_expr;
-      type_statement (Environment.add_variable env id Type_int) stmt
-      | Foreach (id, list_expr, stmt, _) ->
-        let list_type = type_expression env list_expr in
-        (match list_type with
-         | Type_list elem_type -> type_statement (Environment.add_variable env id elem_type) stmt
-         | _ -> raise (TypeError "Foreach expects a list"))
-      | Draw (expr, _) ->
-        let expr_type = type_expression env expr in
-        (match expr_type with
-         | Type_pos | Type_point -> ()
-         | _ -> raise (TypeError "Draw expects a pos or point"))
-      | Print (expr, _) ->
-        type_expression env expr;
-        ()
-      | Nop -> ()
-  
-    match program with
-    | Program (args, stmt) ->
-      let env = Environment.add_layer type_environment in
-      List.iter (fun (Argument (name, t, _)) ->
-        let env = Environment.add_variable env name t in ()) args;
-      type_statement env stmt
-  
+        let _ = type_expression env e1 report in
+        let _ = type_expression env e2 report in
+        Annotation.set_type annotation Type_pos
+      | Point (e1, e2, annotation) ->
+        let _ = type_expression env e1 report in
+        let _ = type_expression env e2 report in
+        Annotation.set_type annotation Type_point
+      | Color (e1, e2, e3, annotation) ->
+        let _ = type_expression env e1 report in
+        let _ = type_expression env e2 report in
+        let _ = type_expression env e3 report in
+        Annotation.set_type annotation Type_color
+      | Variable (name, annotation) ->
+        (match Environment.get env name with
+        | Some type_expr ->
+          let a = type_expr in
+          Annotation.set_type annotation a
+        | None -> 
+          Error_report.add_error report
+            (Format.sprintf "Variable %s undeclared" name, Annotation.get_pos annotation);
+          let empty_annotation = Annotation.create (Lexing.dummy_pos, Lexing.dummy_pos) in
+          empty_annotation)
+      | Binary_operator (_, e1, e2, annotation) ->
+        let te1 = type_expression env e1 report in
+        let te2 = type_expression env e2 report in
+        let t1 = Annotation.get_type te1 in
+        let t2 = Annotation.get_type te2 
+        if t1 <> t2 then begin
+          Error_report.add_error report
+            (Format.sprintf "Type mismatch: expected %s, but got %s" (Type.to_string t1) (Type.to_string t2), Annotation.get_pos annotation);
+          let empty_annotation = Annotation.create (Lexing.dummy_pos, Lexing.dummy_pos) in
+          empty_annotation
+        end else begin
+          Annotation.set_type annotation t1
+        end
+      | Unary_operator (e, annotation) ->
+        let t = type_expression env e report in
+        (* Add proper type checking for unary operators *)
+        Annotation.set_type annotation Annotation.get_type t; (* Replace Type_int with the correct type based on the operator *)
+      | Field_accessor (_, e, annotation) ->
+        let _ = type_expression env e report in
+        (* Add proper type checking for field accessors *)
+        Annotation.set_type annotation Type_int (* Replace Type_int with the correct type based on the accessor *)
+      | List (expr_list, annotation) ->
+        List.iter (fun e -> ignore (type_expression env e report)) expr_list;
+        (* Add proper type checking for lists *)
+        Annotation.set_type annotation (Type_list Type_int) (* Replace Type_int with the correct type based on the list elements *)
+      | Cons (e1, e2, annotation) ->
+        let _ = type_expression env e1 report in
+        let _ = type_expression env e2 report in
+        (* Add proper type checking for cons *)
+        Annotation.set_type annotation (Type_list Type_int) (* Replace Type_int with the correct type based on the cons elements *)
+      in 
+      let process_expression expr =
+        ignore (type_expression type_environment expr report)
+      in
+    
+      List.iter process_expression program;
+      report
